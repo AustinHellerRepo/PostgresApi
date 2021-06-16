1644b343ed1dccceb672a746a234e0b11b979494
austin <galaky2005@gmail.com>
2021-06-15 21:20:53 -0400

    testing polling queue insert to front;

--- python/unit_test/database.py
+++ python/unit_test/database.py
@@ -4,6 +4,8 @@ from unittest.mock import patch
 from core.database import DatabaseCommandPollingExecutableQueue, DatabaseInterface, Callback, ExecutableElement, CommandResult, DefaultCommandResult
 from core.callback import FunctionCallback, JsonConvertable
 from core.queue import DefaultExecutableElement, DelegatedExecutableElement
+from typing import List
+import time
 
 
 class TestDatabaseCommandPollingExecutableQueue(unittest.TestCase):
@@ -30,6 +32,7 @@ class TestDatabaseCommandPollingExecutableQueue(unittest.TestCase):
 			return None
 
 		_database_interface = DatabaseInterface()
+
 		_execution_result_callback = FunctionCallback(
 			function=_function_callback
 		)
@@ -56,6 +59,146 @@ class TestDatabaseCommandPollingExecutableQueue(unittest.TestCase):
 		_database_command_polling_executable_queue.wait_until_empty()
 
 		_database_command_polling_executable_queue.dispose()
+
+	@patch.multiple(DatabaseInterface, __abstractmethods__=set())
+	def test_insert_order_respected_sequential_and_fast(self):
+
+		_inserts_total = 10
+
+		def _get_json_per_index(index: int) -> str:
+			return f'{{ "index": {index} }}'
+
+		_order_of_callback = []  # type: List[int]
+
+		def _function_callback(data: object) -> JsonConvertable:
+			_order_of_callback.append(data)
+			return None
+
+		_database_interface = DatabaseInterface()
+
+		_execution_result_callback = FunctionCallback(
+			function=_function_callback
+		)
+
+		_database_command_polling_executable_queue = DatabaseCommandPollingExecutableQueue(
+			database_interface=_database_interface,
+			execution_result_callback=_execution_result_callback
+		)
+
+		for _index in range(_inserts_total):
+
+			_executable_element = DefaultExecutableElement(
+				default_output=DefaultCommandResult(
+					default_json=_get_json_per_index(_index)
+				)
+			)
+
+			_database_command_polling_executable_queue.insert_at_front_immediately(
+				executable_element=_executable_element
+			)
+
+		_database_command_polling_executable_queue.wait_until_empty()
+
+		_database_command_polling_executable_queue.dispose()
+
+		self.assertEqual(len(_order_of_callback), _inserts_total)
+		for _index in range(_inserts_total):
+			self.assertEqual(_order_of_callback[_index], _get_json_per_index(_index))
+
+	@patch.multiple(DatabaseInterface, __abstractmethods__=set())
+	def test_insert_order_respected_sequential_and_slow(self):
+
+		_inserts_total = 5
+
+		def _get_json_per_index(index: int) -> str:
+			return f'{{ "index": {index} }}'
+
+		_order_of_callback = []  # type: List[int]
+
+		def _function_callback(data: object) -> JsonConvertable:
+			_order_of_callback.append(data)
+			return None
+
+		_database_interface = DatabaseInterface()
+
+		_execution_result_callback = FunctionCallback(
+			function=_function_callback
+		)
+
+		_database_command_polling_executable_queue = DatabaseCommandPollingExecutableQueue(
+			database_interface=_database_interface,
+			execution_result_callback=_execution_result_callback
+		)
+
+		for _index in range(_inserts_total):
+			_executable_element = DefaultExecutableElement(
+				default_output=DefaultCommandResult(
+					default_json=_get_json_per_index(_index)
+				)
+			)
+
+			_database_command_polling_executable_queue.insert_at_front_immediately(
+				executable_element=_executable_element
+			)
+
+			time.sleep(0.75)
+
+		_database_command_polling_executable_queue.wait_until_empty()
+
+		_database_command_polling_executable_queue.dispose()
+
+		self.assertEqual(len(_order_of_callback), _inserts_total)
+		for _index in range(_inserts_total):
+			self.assertEqual(_order_of_callback[_index], _get_json_per_index(_index))
+
+	@patch.multiple(DatabaseInterface, __abstractmethods__=set())
+	def test_insert_order_respected_sequential_and_slower(self):
+
+		_inserts_total = 5
+
+		def _get_json_per_index(index: int) -> str:
+			return f'{{ "index": {index} }}'
+
+		_order_of_callback = []  # type: List[int]
+
+		def _function_callback(data: object) -> JsonConvertable:
+			_order_of_callback.append(data)
+			return None
+
+		_database_interface = DatabaseInterface()
+
+		_execution_result_callback = FunctionCallback(
+			function=_function_callback
+		)
+
+		_database_command_polling_executable_queue = DatabaseCommandPollingExecutableQueue(
+			database_interface=_database_interface,
+			execution_result_callback=_execution_result_callback
+		)
+
+		for _index in range(_inserts_total):
+			_executable_element = DefaultExecutableElement(
+				default_output=DefaultCommandResult(
+					default_json=_get_json_per_index(_index)
+				)
+			)
+
+			_database_command_polling_executable_queue.insert_at_front_immediately(
+				executable_element=_executable_element
+			)
+
+			time.sleep(1.25)
+
+		_database_command_polling_executable_queue.wait_until_empty()
+
+		_database_command_polling_executable_queue.dispose()
+
+		self.assertEqual(len(_order_of_callback), _inserts_total)
+		for _index in range(_inserts_total):
+			self.assertEqual(_order_of_callback[_index], _get_json_per_index(_index))
+
+	def test_set_of_commands_for_creating_inserting_and_pulling_data(self):
+		pass
 
 
 if __name__ == "__main__":
